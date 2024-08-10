e4943b24b5e2cb48267e4bd3a053b4107b549094
z$hr@InTheMiddle <83715370+Zehrakmnl@users.noreply.github.com>
2024-08-10 19:19:58 +0300

    Add files via upload

---
+++ 1.rb
@@ -0,0 +1,24 @@
+puts "HEllo"
+=begin
+multi line 
+command
+on ruby 
+=end
+
+puts "Hi i am #{3*7} "
+name = "Zehra"
+surname = "Kamanli"
+puts "My name is #{name} and surname is #{surname}"
+
+puts"so what is your name, who are you?"
+name2 = gets.chomp
+puts"ou are you my mother #{name2} "
+answ = gets.chomp
+puts"Wow i am suprised. Nice to meet you mum "
+puts"okey but what is your age?"
+age = gets.chomp.to_i
+puts " mm #{age} not old. You are steel young :)"
+puts "How many kilos are you?"
+weight = gets.chomp.to_f
+puts "wow did you eat a house. i'm not even #{weight}"
+
